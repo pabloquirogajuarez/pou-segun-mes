fe614d3000ab05bb372d1b7209286eea266827b5
pabloquirogajuarez <pabloquirogajuarez@gmail.com>
2024-05-20 13:58:35 -0300

    ui/ux changes

--- src/app/components/imagen-mes/imagen-mes.component.ts
+++ src/app/components/imagen-mes/imagen-mes.component.ts
@@ -7,8 +7,24 @@ import { Component } from '@angular/core';
 })
 export class ImagenMesComponent {
   mesSeleccionado: string = ''; // Variable para almacenar el mes seleccionado
-  
-  meses: string[] = ['enero', 'febrero', 'marzo', 'abril', 'mayo', 'junio', 'julio', 'agosto', 'septiembre', 'octubre', 'noviembre', 'diciembre']; // Arreglo con los nombres de los meses
+  meses: string[] = ['enero', 'febrero', 'marzo', 'abril', 'mayo', 'junio', 'julio', 'agosto', 'septiembre', 'octubre', 'noviembre', 'diciembre']; // arreglo con los nombres de los meses
+  liked: boolean = false;
+  likes: number = 0;
 
   constructor() { }
+
+  compartirEnTwitter() {
+    const url = 'https://pabloquirogajuarez.github.io/pou-segun-mes/'; 
+    const texto = `Descubrí que pou sos según tu mes de nacimiento 👀.
+     Mi mes es ${this.mesSeleccionado}!`;
+    const twitterUrl = `https://x.com/intent/tweet?url=${encodeURIComponent(url)}&text=${encodeURIComponent(texto)}`;
+    window.open(twitterUrl, '_blank');
+  }
+
+  darLike() {
+    this.liked = !this.liked;
+    this.likes += this.liked ? 1 : -1;
+  }
+
 }
+
